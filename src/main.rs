e4d83b4603b5dd875676430ade5dae533c5534e7
raimon <raimon49@hotmail.com>
2020-12-11 17:48:54 +0900

    Add a code about use std::io::Write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::Write;
+
+fn say_hello(out: &mut dyn Write) -> std::io::Result<()> {
+    out.write_all(b"hello, world\n")?;
+    out.flush()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut bytes = vec![];
+    say_hello(&mut bytes);
+    assert_eq!(bytes, b"hello, world\n");
 }
