46741cfbc62d452042c96930c8c188c5e9117370
raimon <raimon49@hotmail.com>
2020-12-12 14:40:31 +0900

    Add a code about using Vec<u8> as a implemented std::io::Write

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,11 @@ fn min<T: Ord>(value1: T, value2: T) -> T {
 
 fn main() {
     let mut bytes = vec![];
-    say_hello(&mut bytes);
+    let _ = say_hello(&mut bytes);
     assert_eq!(bytes, b"hello, world\n");
+
+    let mut buf: Vec<u8> = vec![];
+    let _ = buf.write_all(b"hello");
 
     let _ = min(5, 4);
 }
