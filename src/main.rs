ef9cd4a815bb2f82d8c5882e72908125392d4610
raimon <raimon49@hotmail.com>
2020-12-12 14:37:40 +0900

    Define function 'min'

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,18 @@ fn say_hello(out: &mut dyn Write) -> std::io::Result<()> {
     out.flush()
 }
 
+fn min<T: Ord>(value1: T, value2: T) -> T {
+    if value1 <= value2 {
+        value1
+    } else {
+        value2
+    }
+}
+
 fn main() {
     let mut bytes = vec![];
     say_hello(&mut bytes);
     assert_eq!(bytes, b"hello, world\n");
+
+    let _ = min(5, 4);
 }
