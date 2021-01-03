5c7595119881150dbd674bdbd404a1e935200bdf
raimon <raimon49@hotmail.com>
2021-01-03 17:55:38 +0900

    Add a test code about using dot()

--- src/main.rs
+++ src/main.rs
@@ -196,3 +196,8 @@ fn dot<N>(v1: &[N], v2: &[N]) -> N
     total
 }
 
+#[test]
+fn test_dot() {
+    assert_eq!(dot(&[1, 2, 3, 4], &[1, 1, 1, 1]), 10);
+    assert_eq!(dot(&[53.0, 7.0], &[1.0, 5.0]), 88.0);
+}
